28d5707d86ce0290ae6b8aae984d16e84af19d88
Wellington Felix <warlindo0@gmail.com>
2026-05-21 14:16:50 -0300

    Estrutura inicial do projeto

--- src/main/java/com/projeto/soci/repository/UsuarioRepository.java
+++ src/main/java/com/projeto/soci/repository/UsuarioRepository.java
@@ -1,2 +1,8 @@
+package com.projeto.soci.repository;
 
+import com.projeto.soci.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
